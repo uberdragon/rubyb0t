42d1d3f9930555cf13ea866e392ebaba110f87f9
uberdragon <dragonscmc@gmail.com>
2013-08-28 02:27:59 -0500

    added ability to retrieve all 10 results on page by adding number at end of query

--- lib/google.rb
+++ lib/google.rb
@@ -8,18 +8,29 @@ class Google
   match /google (.+)/
 
   def search(query)
-    url = "http://www.google.com/search?q=#{CGI.escape(query)}"
-
     if query.split.last == 'url'
-      url[0..-5]
+      query = query.split[0...-1].join(" ")
+      return "http://www.google.com/search?q=#{CGI.escape(query)}"
     else
+      if query.split.pop.to_i > 0
+        num = query.split.pop.to_i
+        query = query.split[0...-1].join(" ")
+      end
+      url = "http://www.google.com/search?q=#{CGI.escape(query)}"
+      if num.to_i > 0
+        s = num - 1
+      else
+        s = 0
+      end
+
       page = Nokogiri::HTML(open(url))
-      title = page.at("h3.r").text
-      desc = page.at("span.st").text
-      link = page.at("h3.r").at('a')[:href].gsub('/url?q=','').split('&')[0]
+      title = page.css("h3.r")[s].text
+      desc = page.css("span.st")[s].text
+      link = page.css("h3.r")[s].at('a')[:href].gsub('/url?q=','').split('&')[0]
 
       CGI.unescape_html "#{title} - #{desc} (#{link})"
     end
+
   rescue
     "No results found"
   end
