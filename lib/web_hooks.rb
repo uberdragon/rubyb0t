1b46b57416c269a7eea8e088c982bed0f5e81b8c
uberdragon <dragonscmc@gmail.com>
2013-09-09 20:08:40 -0500

    Added some base web page hooks with comments for other developers to know the purpose of the hook

--- lib/web_hooks.rb
+++ lib/web_hooks.rb
@@ -20,14 +20,34 @@ end
 
 # Listen to sinatra URLs
 
+# This needs to be protected and probably moved into the /cmd area
+# Currently allows anyone to send a /announce?message=<message here> which will
+# /amsg all channels the bot is in with the message received.
 get '/announce' do
 	WebHooks.announce params[:message]
 end
 
+# Protected behind password area; this should give all kinds of stats the bot has collected
+# such as ability to get bot feed back from triggers that store information; ie: !seen !top10 etc
 get '/stats' do
 	erb "wtf"
 end
 
+# Perhaps some data about the channels bot is in with ability
+# to select a specific channel.  Channel relay to web, IRC @op commands, nick list etc accessible
+get '/channels' do
+
+end
+
+
+# Intended to be an API to any command the bot can do based on permissions.
+# should accept commands via POST in JSON format
+get '/cmd' do
+
+end
+
+# Bot's index page - could show general stats for the public and then contain
+# a login area to be able to control the bot and use it to channel spy etc
 get '/' do
 	erb "showing index y0"
 end
